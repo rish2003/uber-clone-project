2e753b325571dc25a04b36e66b586e5464bac1ac
Rish2003 <rishabhraj6421@gmail.com>
2024-12-26 14:16:48 +0530

    RegisterCaptain UI and UserContext Created

---
+++ client/src/context/UserContext.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
+import { createContext, useState } from "react";
+
+export const UserDataContext = createContext();
+
+const UserContext = ({ children }) => {
+  const [user, setUser] = useState({
+    email: "",
+    fullName: {
+      firstName: "",
+      lastName: "",
+    },
+  });
+
+  return (
+    <div>
+      <UserDataContext.Provider value={{ user, setUser }}>
+        {children}
+      </UserDataContext.Provider>
+    </div>
+  );
+};
+
+export default UserContext;
